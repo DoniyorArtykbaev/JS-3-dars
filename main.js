0432c2aa93ce54fd0bc0bf4e817d942fa89830ee
DoniyorArtykbaev <donerartykbaev@gmail.com>
2024-06-04 08:23:06 +0500

    JS 3-dars
    Zeromax IT-Center 35-guruh

---
+++ main.js
@@ -0,0 +1,105 @@
+
+
+//let matn = "salom";
+//let raqam = 1234;
+//let soz;
+//document.write(typeof matn)
+
+//let rost = true;
+
+//document.write(typeof rost)
+
+//let rost = 45 == 45 ? true:false; 
+
+//document.write(rost)
+
+//let arrayObject = [45,78,"salom","matn",true,folse,"javascript","olma",123];
+
+//console.log(arrayObject[]);
+
+//let oddiyObject = {ism: "Doni", yoshi:14,mahallasi:"Amir Temur mahallasi"}
+
+//console.log(oddiyObject)
+
+//let savatcha = [12,78,"matn",true];
+//document.write(savatcha);
+
+//function savatchaniTekshir(){
+//    if(savatcha.length == 0){
+//        document.write("Savatcha ichida" + savatcha.length + "dona ma'lumot bor");
+//    }
+//    else{
+//        document.write("Savatca ichida" + savatcha.length + "ta ma'lumot bor") 
+//    }
+//}
+//let savatcha = [12,true,78,"matn","true"];
+//savatchaniTekshir(continur);
+
+let sana = new Date();  
+
+let yil = sana.getFullYear(); 
+let oy = sana.getMonth();
+let kun = sana.getDate();
+let haftaKuni = sana.getDay();
+  
+ let oylar = ["yanvar", "fevrel","mart","aprel","may","iyun","iyul","avgust","sentyabr","oktyabr","noyabr","dekabr"];
+ let haftalar =["dushanba","seshanba","chorshanba","payshanba","juma","shanba","yakshanba"]
+
+document.write("" + yil + "-yil" + oylar[oy] + " oyning " + kun + " -kuni.  Haftaning" + haftaKuni + "-kuni."); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
